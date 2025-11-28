1a378af78eff448e511bdf00862176e24b7a505e
Arpan Mandaviya <arpanmandviya.cs@gmail.com>
2025-11-28 12:48:22 +0530

    system 0.0.1

--- src/GeneratorServiceProvider.php
+++ src/GeneratorServiceProvider.php
@@ -1,32 +1,20 @@
 <?php
 
-
 namespace Arpanmandaviya\SystemBuilder;
 
-
 use Illuminate\Support\ServiceProvider;
-use Arpan\SystemBuilder\Commands\BuildSystemCommand;
-
 
 class GeneratorServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                \Arpanmandaviya\SystemBuilder\Commands\BuildSystemCommand::class,
-            ]);
-
-
-            $this->publishes([
-                __DIR__ . '/../resources/stubs' => base_path('system-builder-stubs'),
-            ], 'system-builder-stubs');
-        }
-    }
-
-
     public function register()
     {
-// merge config later if needed
+        $this->commands([
+            \Arpanmandaviya\SystemBuilder\Commands\BuildSystemCommand::class,
+        ]);
+    }
+
+    public function boot()
+    {
+        // boot logic here
     }
 }
