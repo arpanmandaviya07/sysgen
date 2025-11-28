87af8a750d0064d097a6ced27d906b8119f513cb
Arpan Mandaviya <arpanmandviya.cs@gmail.com>
2025-11-28 16:34:16 +0530

    system 1.0.9.1

---
+++ src/SystemBuilderServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Arpanmandaviya\SystemBuilder;
+
+use Illuminate\Support\ServiceProvider;
+use Arpanmandaviya\SystemBuilder\Commands\BuildSystemCommand;
+use Arpanmandaviya\SystemBuilder\Commands\InteractiveBuildCommand;
+
+class SystemBuilderServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                BuildSystemCommand::class,
+                InteractiveBuildCommand::class,
+            ]);
+        }
+    }
+}
